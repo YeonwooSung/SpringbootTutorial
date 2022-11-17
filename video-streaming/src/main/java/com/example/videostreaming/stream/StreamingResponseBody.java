9da3513269f35815149a3b4ccc28129f599bdc80
YeonwooSung <neos960518@gmail.com>
2022-11-17 23:34:49 +0900

    Remove unnecessary interface

--- video-streaming/src/main/java/com/example/videostreaming/stream/StreamingResponseBody.java
+++
@@ -1,9 +0,0 @@
-package com.example.videostreaming.stream;
-
-import java.io.IOException;
-import java.io.OutputStream;
-
-@FunctionalInterface
-public interface StreamingResponseBody {
-    void writeTo(OutputStream outputStream) throws IOException;    
-}
