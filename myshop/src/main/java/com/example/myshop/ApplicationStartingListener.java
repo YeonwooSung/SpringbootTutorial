5ba84b1155c9b10be99ad7e540e1725445436301
YeonwooSung <neos960518@gmail.com>
2024-04-27 14:29:37 +0900

    init myshop pjt

---
+++ myshop/src/main/java/com/example/myshop/ApplicationStartingListener.java
@@ -0,0 +1,12 @@
+package com.example.myshop;
+
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
+
+public class ApplicationStartingListener implements ApplicationListener<ApplicationStartingEvent> {
+
+    @Override
+    public void onApplicationEvent(ApplicationStartingEvent event) {
+        System.out.println("Handling ApplicationStartingEvent here!");
+    }
+}
---
+++ myshop/src/main/java/com/example/myshop/MyshopApplication.java
@@ -0,0 +1,17 @@
+package com.example.myshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.myshop.ApplicationStartingListener;
+
+@SpringBootApplication
+public class MyshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(MyshopApplication.class);
+		app.addListeners(new ApplicationStartingListener());
+		app.run(args);
+	}
+
+}
